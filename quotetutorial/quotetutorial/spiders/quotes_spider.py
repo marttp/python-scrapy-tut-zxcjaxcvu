0f62c2527906ff1c8160af2db5afe07687f4b84f
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-10-30 03:25:27 +0700

    How to get data from next page until none

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -20,3 +20,7 @@ class QuotesSpider(scrapy.Spider):
       items['author'] = author
       items['tag'] = tag
       yield items
+
+    next_page = response.css('li.next a::attr(href)').get()
+    if next_page is not None:
+      yield response.follow(next_page, callback = self.parse)
