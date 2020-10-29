9b3842d8b49a5e5bb79c0c356b0e6c2b97915d61
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-10-29 23:32:33 +0700

    first commit

---
+++ quotetutorial/quotetutorial/items.py
@@ -0,0 +1,16 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+# Extract data -> Temporary containers (items) -> Storing in database
+
+import scrapy
+
+
+class QuotetutorialItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    author = scrapy.Field()
+    tag = scrapy.Field()
---
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -0,0 +1,22 @@
+import scrapy
+from ..items import QuotetutorialItem
+
+class QuotesSpider(scrapy.Spider):
+  name = 'quotes'
+  start_urls = [
+    'http://quotes.toscrape.com/'
+  ]
+
+  def parse(self, response):
+
+    items = QuotetutorialItem()
+    all_div_quotes = response.css('div.quote')
+
+    for quote in all_div_quotes:
+      title = quote.css('span.text::text').extract()
+      author = quote.css('.author::text').extract()
+      tag = quote.css('.tag::text').extract()
+      items['title'] = title
+      items['author'] = author
+      items['tag'] = tag
+      yield items
