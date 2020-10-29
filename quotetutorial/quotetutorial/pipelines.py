3c42da512aa60aec0adcfdb9c100deaa77c0ffb3
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-10-29 23:48:30 +0700

    add simple pipeline

--- quotetutorial/quotetutorial/pipelines.py
+++ quotetutorial/quotetutorial/pipelines.py
@@ -3,6 +3,9 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+# Scraped data -> Item Containers -> Json/Csv files
+# Scraped data -> Item Containers -> Pipeline -> SQL/Mongo Database
+
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
@@ -10,4 +13,5 @@ from itemadapter import ItemAdapter
 
 class QuotetutorialPipeline:
     def process_item(self, item, spider):
+        print("Pipeline :" + item['title'][0])
         return item
