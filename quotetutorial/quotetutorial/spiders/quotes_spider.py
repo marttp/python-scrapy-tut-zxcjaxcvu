87b37b0527a28861e5c6459548a9c3d186da1026
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-10-30 03:29:13 +0700

    How to use with paging or param

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -3,8 +3,9 @@ from ..items import QuotetutorialItem
 
 class QuotesSpider(scrapy.Spider):
   name = 'quotes'
+  page_number = 2
   start_urls = [
-    'http://quotes.toscrape.com/'
+    'http://quotes.toscrape.com/page/1/'
   ]
 
   def parse(self, response):
@@ -21,6 +22,7 @@ class QuotesSpider(scrapy.Spider):
       items['tag'] = tag
       yield items
 
-    next_page = response.css('li.next a::attr(href)').get()
-    if next_page is not None:
+    next_page = 'http://quotes.toscrape.com/page/{}/'.format(QuotesSpider.page_number)
+    if QuotesSpider.page_number <= 11:
+      QuotesSpider.page_number += 1
       yield response.follow(next_page, callback = self.parse)
