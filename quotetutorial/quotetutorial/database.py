be8495a68db1a938807e47c79f7d01e5e1308500
Thanaphoom Babparn <tp.babparn@gmail.com>
2020-10-30 00:07:50 +0700

    Add logic to add into table

---
+++ quotetutorial/quotetutorial/database.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+conn = sqlite3.connect('myquotes.db')
+curr = conn.cursor()
+
+# curr.execute("""create table quotes_tb(title text, author text, tag text)""")
+
+curr.execute("""insert into quotes_tb values ()""")
+
+conn.commit()
+conn.close()
